adaf13c6a737cc8ddc7db97b0e3c1202021431e1
Rino Alfian <rino.alpin@gmail.com>
2021-10-28 09:41:06 +0700

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 def tcp_handler(data):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('hli-tcp-router-apim.apps.ocp-dev.hanabank.co.id', 32449))
+    client_socket.connect(('web', 32449))
     client_socket.send(bytes(data, encoding='utf-8'))
     reply = client_socket.recv(131072)
     return reply.decode()
